286d1a5538d1502376b809b8382585dea08dad04
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-10-08 23:12:05 +0530

    Add files via upload
    init

---
+++ kotlin-quarku-helloworld/src/main/kotlin/com/knf/dev/demo/GreetingResource.kt
@@ -0,0 +1,15 @@
+package com.knf.dev.demo
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/hello")
+class GreetingResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello(): String {
+        return "Hello World"
+    }
+}
